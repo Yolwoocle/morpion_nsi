fb821fb33248a38727c9c21a8e7b9998a593eb1b
Yauyau123 <tranguillaume16@gmail.com>
2021-12-13 13:46:19 +0100

    premier jeu

---
+++ morpion.py
@@ -0,0 +1,14 @@
+class Joueur:
+    def __init__(self,nom,symb):
+        self.nom = nom
+        self.symb = symb
+    
+    def __str__
+
+class Grille:
+    def __init__(self,table):
+        self.table = table
+    
+    def is_empty(n):
+        return table[n] == None
+    def change
