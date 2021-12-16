12520f41dc126d957162ca245b49ed1b332c20fa
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2021-12-16 14:54:28 +0100

    Fix boutons

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ class Bouton:
 		self.pos = pos
 		self.size = size
 		self.offset = (0, 0)
+		self.survole = False
+		self.bounce = False
 
 	def est_survole(self):
 		souris_x, souris_y = pygame.mouse.get_pos()
@@ -51,8 +53,7 @@ class Bouton:
 		return contact
 
 	def est_clique(self, clic_gauche):
-		bouton_x, bouton_y = self.pos[0], self.pos[1]
-		if self.survole:
+		if self.est_survole():
 			if clic_gauche:
 				return True
 		return False
@@ -76,6 +77,19 @@ class Grille:
 		self.pos_cursor = [-84, -84]
 		self.width_cursor = 0
 	
+	def initialiser_boutons(self):
+		n = self.n
+		l_grille = n * image_size + (n-1) * espace_entre_cases
+		x = (l_ecran - l_grille) / 2
+		y = (h_ecran - l_grille) / 2
+		w = image_size + espace_entre_cases
+
+		self.boutons = []
+		for i in range(n):
+			self.boutons.append([])
+			for j in range(n):
+				self.boutons[i].append(Bouton((i, j), (x + j*w, y + i*w), image_res))
+
 	def est_vide(self, index):
 		return self.table[index[0]][index[1]] == 0
 
@@ -87,6 +101,11 @@ class Grille:
 		if j == None:
 			return self.table[index[0]][index[1]]
 		return self.table[index][j]
+
+	def lire_bouton(self, index, j=None):
+		if j == None:
+			return self.boutons[index[0]][index[1]]
+		return self.boutons[index][j]
 	
 	def victoire(self, tour):
 		table = self.table
@@ -108,23 +127,14 @@ class Grille:
 				if self.valeur(pos) != last_char:
 					aligne = False
 			if aligne and last_char != 0:
+				# En cas de victoire
+				for pos in comb:
+					self.lire_bouton(pos).bounce = True 
 				return last_char
+
 		if tour >= n*n:
 			return "match nul"
 		return False
-	
-	def initialiser_boutons(self):
-		n = self.n
-		l_grille = n * image_size + (n-1) * espace_entre_cases
-		x = (l_ecran - l_grille) / 2
-		y = (h_ecran - l_grille) / 2
-		w = image_size + espace_entre_cases
-
-		self.boutons = []
-		for i in range(n):
-			self.boutons.append([])
-			for j in range(n):
-				self.boutons[i].append(Bouton((i, j), (x + j*w, y + i*w), image_res))
 	
 	def interaction_boutons(self, clic_gauche):
 		for ligne in self.boutons:
@@ -153,9 +163,15 @@ class Grille:
 					image = image_x
 				elif case == "o":
 					image = image_o
-				screen.blit(pygame.transform.scale(image, image_res), bouton.pos)
+				
+				x, y = bouton.pos
+				x += bouton.offset[0]
+				y += bouton.offset[1]
+				if bouton.bou
+				
+				screen.blit(pygame.transform.scale(image, image_res), (x, y))
 		# Afficher curseur
-		# TODO: 4 sprites curseur
+		# TODO: sélparer curseur dans sa classe
 		w = image_size / 2
 		corners = ((0,0,w,w),(0,w,w,w),(w,0,w,w),(w,w,w,w))
 		offset = ((-1,-1),(-1,1),(1,-1),(1,1))
@@ -227,7 +243,6 @@ class Jeu:
 			text = ""
 			if victoire:
 				text = f"{victoire} a gagné!"
-				print(f"{victoire} a gagné!")
 			textsurface = small_font.render(text, False, (0, 0, 0))
 			screen.blit(textsurface,(0,0))
 
